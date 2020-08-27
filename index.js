b43642e2b0967cd177226a320e0416ea34d9f6fe
Jordan Muncrief <jordean89@gmail.com>
2020-08-27 16:07:21 -0600

    Fixed functions; applications now runs. MVP achieved.

--- index.js
+++ index.js
@@ -3,19 +3,17 @@ const generateMarkdown = require("./utils/generateMarkdown.js");
 const fs = require("fs");
 
 // function to write README file
-async function writeToFile(fileName, data) {
-    fs.writeFile(fileName + ".md", data);
-}
+function writeToFile(fileName, data,) {
+    fs.writeFile(fileName + ".md", data, function(err){
+        console.log("Complete.")}
+    )}
 
 // function to initialize program
-function init() {
-    let data = prompts();
+async function init() {
+    let data = await prompts();
     let md = generateMarkdown(data);
-    writeToFile("Test Title", md, function(err){
-        console.log("Complete.")
-    });
-
-}
+    writeToFile(data.title, md)
+};
 
 // function call to initialize program
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,5 @@
 // function to generate markdown for README
-async function generateMarkdown({title, description, installation, usage, license, contributing, tests, questions}) {
+function generateMarkdown({title, description, installation, usage, license, contributing, tests, questions}) {
   return `
 
 ![license badge](https://img.shields.io/badge/license-${license}-blue)
@@ -8,13 +8,13 @@ async function generateMarkdown({title, description, installation, usage, licens
 
 ## Table of Contents
 
-*[Description](#description)
-*[Installation](#installation)
-*[Usage](#usage)
-*[License](#license)
-*[Contributing](#contributing)
-*[Tests](#tests)
-*[Questions](#questions)
+*[Description](#description)\n
+*[Installation](#installation)\n
+*[Usage](#usage)\n
+*[License](#license)\n
+*[Contributing](#contributing)\n
+*[Tests](#tests)\n
+*[Questions](#questions)\n
 
 ## Description
 
